84036674b7c38b5f9e6bb2efbbcb2e759964ddbf
Kfir <kfir.carmely2@gmail.com>
2025-03-30 02:53:29 +0300

    Created A Browser Class to handle requests and responses. seperated into functions

--- browser_web_lightweight.py
+++ browser_web_lightweight.py
@@ -1,9 +1,28 @@
 import socket
 import client
+from urllib.parse import urlparse
+import os
 
-while True:
-    url = input("Please enter the URL of the website you to access: \n")
-    server = client.Client(url)
+class Browser:
+    def __init__(self):
+        self.client = client.Client()
+        self.url = input("Please enter the URL of the website you to access: \n")
+        self.fixed_url = urlparse(self.url).netloc
+        self.path = urlparse(self.url).path
+        self.sources = []
 
-    server.connect()
-    request = f"GET /{url} HTTP/1.0\r\n Host:"
+
+    def access_server(self): # Gets The URL and adresses the server
+        ip = socket.gethostbyname(self.fixed_url)
+
+        self.client.connect(ip)
+
+        self.send_request("GET")
+
+        data = self.client.recv(2048)
+        print(data.decode())
+
+    def send_request(self, req_type):
+        if req_type == "GET":
+            request = f"GET {self.path} HTTP/1.1\r\nHost:www.{self.fixed_url}\r\n\r\n"
+            self.client.send(request.encode())
--- client.py
+++ client.py
@@ -1,8 +1,7 @@
 import socket
 
 class Client: # Class that handles the socket
-    def __init__(self, url): # Constructor
-        self.url = url
+    def __init__(self): # Constructor
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # The client, sends request to server
 
     def send(self, message): # Sends a message to the client
@@ -11,8 +10,8 @@ class Client: # Class that handles the socket
     def recv(self, amount): # Receives certain amount of bytes
         return self.client_socket.recv(amount)
 
-    def connect(self): # start the client
-        self.client_socket.connect((self.url, 80))
+    def connect(self, ip): # start the client
+        self.client_socket.connect((ip, 80))
 
     def close(self): # Closes the client
         self.client_socket.close()
